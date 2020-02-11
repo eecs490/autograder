ea28baaf8714bdbabe3c38f3150929cd7fbe3c4e
Ethan Brooks <ethanabrooks@gmail.com>
2020-02-11 02:15:40 -0500

    clean up json

--- submission/src/main.rs
+++ submission/src/main.rs
@@ -1,27 +1,27 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-const JSON_STRING: &str = r#"{ "score": 44.0, // optional, but required if not on each test case below. Overrides total of tests if specified.
-  "execution_time": 136, // optional, seconds
-  "output": "Text relevant to the entire submission", // optional
-  "visibility": "after_due_date", // Optional visibility setting
-  "stdout_visibility": "visible", // Optional stdout visibility setting
-  "extra_data": {}, // Optional extra data to be stored
-  "tests": // Optional, but required if no top-level score
+const JSON_STRING: &str = r#"{ 
+  "score": 44.0,
+  "execution_time": 136,
+  "output": "Text relevant to the entire submission",
+  "visibility": "after_due_date",
+  "stdout_visibility": "visible",
+  "extra_data": {},
+  "tests":
     [
         {
-            "score": 2.0, // optional, but required if not on top level submission
-            "max_score": 2.0, // optional
-            "name": "Your name here", // optional
-            "number": "1.1", // optional (will just be numbered in order of array if no number given)
-            "output": "Giant multiline string that will be placed in a <pre> tag and collapsed by default", // optional
-            "tags": ["tag1", "tag2", "tag3"], // optional
-            "visibility": "visible", // Optional visibility setting
-            "extra_data": {} // Optional extra data to be stored
+            "score": 2.0,
+            "max_score": 2.0,
+            "name": "Your name here",
+            "number": "1.1",
+            "output": "Giant multiline string that will be placed in a <pre> tag and collapsed by default",
+            "tags": ["tag1", "tag2", "tag3"],
+            "visibility": "visible",
+            "extra_data": {}
         },
-        // and more test cases...
     ],
-  "leaderboard": // Optional, will set up leaderboards for these values
+  "leaderboard":
     [
       {"name": "Accuracy", "value": .926},
       {"name": "Time", "value": 15.1, "order": "asc"},
