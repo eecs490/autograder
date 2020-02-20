e6da1eee24b0b1b013edd726d3c8c65ca12f2619
Ethan Brooks <ethanabrooks@gmail.com>
2020-02-20 11:03:57 -0500

    change comments and error messages

--- autograder/assignment/src/main.rs
+++ autograder/assignment/src/main.rs
@@ -23,13 +23,17 @@ fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let submission_path = args
         .get(1)
-        .expect("Must provide one argument representing path to submission Cargo.toml.");
+        .expect("Must provide one argument representing path to submission/Cargo.toml.");
     let assignment_path = args
         .get(2)
-        .expect("Must provide one argument representing path to assignment Cargo.toml.");
+        .expect("Must provide one argument representing path to assignment/Cargo.toml.");
     let output_path = args
         .get(3)
         .expect("Must provide one argument representing path to write results file.");
-    let scores: HashMap<String, f32> = map! { "tests::test4" => 5.0 }; // assign custom scores to each test function. The autograder defaults to 1.0 point per test for tests not included in thei HashMap.
+
+    // assign custom scores to each test function.
+    // The autograder defaults to 1.0 point per test for tests not included in thei HashMap.
+    let scores: HashMap<String, f32> = map! { "tests::test4" => 5.0 };
+
     test_lib::write_report(scores, submission_path, assignment_path, output_path)
 }
--- autograder/test_lib/src/main.rs
+++ autograder/test_lib/src/main.rs
@@ -6,10 +6,10 @@ fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let submission_path = args
         .get(1)
-        .expect("Must provide one argument representing path to submission Cargo.toml.");
+        .expect("Must provide one argument representing path to submission/Cargo.toml.");
     let assignment_path = args
         .get(2)
-        .expect("Must provide one argument representing path to assignment Cargo.toml.");
+        .expect("Must provide one argument representing path to assignment/Cargo.toml.");
     let output_path = args
         .get(3)
         .expect("Must provide one argument representing path to write results file.");
