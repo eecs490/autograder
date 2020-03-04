0aae52ca64ae1daac5d840a9d33add5069906576
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-04 15:42:37 -0500

    Revert "just base report on assignment tests"
    This reverts commit 5966ecf0ac2b6895f6c74d9f94f3d5ceb0152de6.

--- autograder/assignment/src/main.rs
+++ autograder/assignment/src/main.rs
@@ -10,11 +10,14 @@ use std::io::Write;
 
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
-    let assignment_path = args
+    let submission_path = args
         .get(1)
+        .expect("Must provide one argument representing path to submission/Cargo.toml.");
+    let assignment_path = args
+        .get(2)
         .expect("Must provide one argument representing path to assignment/Cargo.toml.");
     let output_path = args
-        .get(2)
+        .get(3)
         .expect("Must provide one argument representing path to write results file.");
 
     // assign custom scores to each test function.
@@ -22,18 +25,27 @@ fn main() -> Result<(), std::io::Error> {
     let scores: HashMap<String, f32> = map! { "tests::test4" => 5.0 };
 
     // scrape cargo test output for assignment and submission
-    let outputs: String = lib::get_test_output(assignment_path.to_string());
-    println!("{}", outputs.clone());
+    let outputs: (String, String) = (
+        lib::get_test_output(assignment_path.to_string()),
+        lib::get_test_output(submission_path.to_string()),
+    );
+    println!("{}", outputs.0.clone());
+    println!("{}", outputs.1.clone());
 
     // deserialize ouputs into TestResult structs
-    let test_results: Vec<TestResult> = lib::get_test_results(outputs);
+    let mut test_results: (Vec<TestResult>, Vec<TestResult>) = (
+        lib::get_test_results(outputs.0),
+        lib::get_test_results(outputs.1),
+    );
+    test_results.0.extend(test_results.1.clone()); //concatenate results
     test_results
+        .0
         .clone()
         .into_iter()
         .for_each(|r| println!("{}", r.to_string()));
 
     // combine TestResult structs into Report struct
-    let report: Report = lib::build_report(test_results, scores);
+    let report: Report = lib::build_report(test_results.0, scores);
     println!("{}", report.clone().to_string());
 
     // write Report object to output_path
