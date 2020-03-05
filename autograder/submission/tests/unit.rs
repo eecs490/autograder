c33d4931ef04b14cc2c4c37e6f78dd5322cea898
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-04 19:01:26 -0500

    break out tests/unit.rs in submission

---
+++ autograder/submission/tests/unit.rs
@@ -0,0 +1,51 @@
+use submission::fib;
+
+#[test]
+fn test0() {
+    assert_eq!(fib(0), 1, "YAY");
+}
+
+#[test]
+fn test1() {
+    assert_eq!(fib(1), 1);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(fib(2), 2);
+}
+
+#[test]
+fn test3() {
+    assert_eq!(fib(3), 3);
+}
+
+#[test]
+fn test4() {
+    // This assert would fire and test will fail.
+    // Please note, that private functions can be tested too!
+    assert_eq!(fib(4), 5, "NOOOOOO");
+}
+
+#[test]
+fn test5() {
+    assert_eq!(fib(5), 8);
+}
+
+#[test]
+fn it_works() -> Result<(), String> {
+    if 2 + 2 == 4 {
+        Ok(())
+    } else {
+        Err(String::from("two plus two does not equal four"))
+    }
+}
+
+//#[test]
+//fn it_doesnt_work() -> Result<(), String> {
+//if 2 + 2 == 3 {
+//Ok(())
+//} else {
+//Err(String::from("two plus two does not equal three"))
+//}
+//}
