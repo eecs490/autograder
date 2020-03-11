2b05455630b1ab6196e286f82ce469a1cb80ffc2
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-11 16:11:17 -0400

    fixed things by adding export commands in config

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
-RUSTUP_HOME=/usr/local/rustup
-CARGO_HOME=/usr/local/cargo
-PATH=/usr/local/cargo/bin:$PATH
+export RUSTUP_HOME=/usr/local/rustup
+export CARGO_HOME=/usr/local/cargo
+export PATH=/usr/local/cargo/bin:$PATH
+
+# these flags are necessary for grcov
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
  
 # file structure
 AUTOGRADER=/autograder/autograder/Cargo.toml
@@ -15,6 +19,3 @@ SCORES=/autograder/scores.yaml
 OUR_SOLUTION=/autograder/assignment/src/solution.rs
 THEIR_SOLUTION=/autograder/submission/src/solution.rs
 
-# these flags are necessary for grcov
-CARGO_INCREMENTAL=0
-RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
