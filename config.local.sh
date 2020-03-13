cbece31a7b5ef11344287bedc724db43fdde99ca
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-13 15:49:57 -0400

    add config.local.sh

---
+++ config.local.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# file structure
+AUTOGRADER=autograder/Cargo.toml
+ASSIGNMENT=assignment/Cargo.toml
+SUBMISSION=submission/Cargo.toml
+OUR_TEST_RESULTS=/tmp/our_test_results
+THEIR_TEST_RESULTS=/tmp/their_test_results
+OUTPUT=/tmp/results.json
+LCOV=/tmp/lcov.info
+SCORES=scores.yaml
+OUR_SOLUTION=assignment/src/solution.rs
+THEIR_SOLUTION=submission/src/solution.rs
+
