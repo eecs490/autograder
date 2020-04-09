5adffa8be95851248f5ec84107c6dce3e3e5bb39
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-09 19:30:33 -0400

    fixed scoring and logging out outputs

--- autograder.py
+++ autograder.py
@@ -18,7 +18,7 @@ def main(build_output_paths: List[Path], scores_path: Path, output: Path):
             ):
                 with output_path.open() as output_file:
                     yield TestReport(
-                        score=1 - int(score),
+                        score=1 if int(score) == 0 else 0,
                         max_score=1,
                         name=f"Problem {i}",
                         label="",
